3f753ffa4a21248b176fab26de03af248a4f56df
Musa Siddeeq <musa@ehirelabs.com>
2013-02-23 13:40:35 -0500

    dynamic map implementation

--- vacantmap.js
+++ vacantmap.js
@@ -1,3 +1,19 @@
+map = new OpenLayers.Map("mapdiv");
+map.addLayer(new OpenLayers.Layer.OSM());
+
+var zoom=12;
+
+var markers = new OpenLayers.Layer.Markers( "Markers" );
+map.addLayer(markers);
+
+var center = new OpenLayers.LonLat( -84.38798240 ,33.74899540 )
+					.transform(
+						new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+						map.getProjectionObject() // to Spherical Mercator Projection
+					);
+					
+map.setCenter (center, zoom);
+
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'properties.php');
 xhr.onload = function() {
@@ -5,5 +21,15 @@ xhr.onload = function() {
 	var jsonProperties = $.parseJSON(xhr.responseText);
 	vacantProp = jsonProperties[0];
 	console.log(vacantProp); // Works now!
+	
+	$.each(data, function(i, item) {
+		var lonLat = new OpenLayers.LonLat( jsonProperties[i].lon ,jsonProperties[i].lat )
+							.transform(
+								new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+								map.getProjectionObject() // to Spherical Mercator Projection
+							);
+
+		markers.addMarker(new OpenLayers.Marker(lonLat));
+	});
 };
 xhr.send();
