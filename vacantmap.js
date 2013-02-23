4682a13216d2e21e4a32b4b544c96a7090422c2f
Musa Siddeeq <musa@ehirelabs.com>
2013-02-23 13:25:36 -0500

    progress for parsing erro

--- vacantmap.js
+++ vacantmap.js
@@ -2,18 +2,8 @@ var xhr = new XMLHttpRequest();
 xhr.open('GET', 'vacant.json');
 xhr.onload = function() {
 	// map the data
-	var response = $.parseJSON(xhr.responseText);
-	vacantProp = response.per.mapRS[0];
+	var jsonProperties = $.parseJSON(xhr.responseText);
+	vacantProp = jsonProperties.per.mapRS[0];
 	console.log(vacantProp); // Works now!
 };
-
-// if (tests.progress) {
-//   xhr.upload.onprogress = function (event) {
-//     if (event.lengthComputable) {
-//       var complete = (event.loaded / event.total * 100 | 0);
-//       progress.value = progress.innerHTML = complete;
-//     }
-//   }
-// }
-
 xhr.send();
